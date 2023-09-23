79d42fe83fc29bb9dad7882cd30874ae90f0b942
Angel Sebastian barrientos gomez <angelsebastian545@gmail.com>
2023-09-22 21:10:07 -0500

    guardar

---
+++ InventoryProject/src/main/java/com/project/usersInvetory/users.java
@@ -0,0 +1,13 @@
+package com.project.usersInvetory;
+import lombok.Data;
+@Data
+public class users {
+    private String Dni;
+    private String Nombre;
+    private String Apellido;
+    private String Correo;
+    private String Usuario;
+    private String Contraseña;
+
+
+}
